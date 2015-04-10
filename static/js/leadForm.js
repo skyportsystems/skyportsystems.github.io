811fa7becdf55f0e1a9ecd9d0c51575863e91184
alfredkam <kam.alfred@gmail.com>
2015-04-10 14:41:49 -0700

    Commented out ajax

--- static/js/leadForm.js
+++ static/js/leadForm.js
@@ -11,23 +11,24 @@
         $(".lead-form").hide();
     });
 
+    /* Commet out below till we resolve ajax */
     // Captures the event of form submission
     // and post an ajax request to sales force
-    $(".lead-form form.pure-form").submit(function () {
-        // Further documentation here: https://developer.salesforce.com/page/Ajax_Proxy
-        // https://naX.salesforce.com/soap/ajax/32.0/connection.js
-        sforce.connection.remoteFunction({
-            url : "https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8",
-            requestData: $(this).serialize(),
-            method: "POST",
-            onSuccess : function(response) {
-                console.lg('success');
-            },
-            onFailure : function(response) {
-                console.log('error');
-            }
-        });
+    // $(".lead-form form.pure-form").submit(function () {
+    //     // Further documentation here: https://developer.salesforce.com/page/Ajax_Proxy
+    //     // https://naX.salesforce.com/soap/ajax/32.0/connection.js
+    //     sforce.connection.remoteFunction({
+    //         url : "https://www.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8",
+    //         requestData: $(this).serialize(),
+    //         method: "POST",
+    //         onSuccess : function(response) {
+    //             console.lg('success');
+    //         },
+    //         onFailure : function(response) {
+    //             console.log('error');
+    //         }
+    //     });
 
-        return false;
-    });
+    //     return false;
+    // });
 })(window, Zepto);
